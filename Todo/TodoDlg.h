d0a08b5f02e465fcaba7e652a52cad5f7166bce7
seong9566 <dlgustjd9566@gmail.com>
2023-02-16 00:14:21 +0900

    초기 커밋.

---
+++ Todo/TodoDlg.h
@@ -0,0 +1,39 @@
+﻿
+// TodoDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CTodoDlg 대화 상자
+class CTodoDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CTodoDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TODO_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	CListBox m_todo_list;
+	afx_msg void OnBnClickedButtonInsert();
+	CString m_text;
+};
